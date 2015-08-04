63b048dbafcf7f193b38ad4ca196086a0628ccfb
benjamin tillett <benjamin.tillett@gmail.com>
2015-08-04 10:43:44 +0100

    add Todo model and get post and delete routes:

--- server.js
+++ server.js
@@ -16,6 +16,58 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+
+var Todo = mongoose.model('Todo',{
+	text: String
+}
+
+// routes 
+
+app.get('/api/todos',function(req,res){
+	Todo.find(function(err,todos){
+		if(err)
+			res.send(err)
+
+		res.json(todos);
+	});
+});
+
+app.post('/api/todos',function(req,res){
+	Todo.create({
+		text: req.body.text,
+		done: false
+	}, function(err,todo){
+		if(err)
+			res.send(err);
+
+		Todo.find(function(err,todos){
+			if(err)
+				res.send(err)
+			res.json(todos);
+		});
+		
+	});
+});
+
+app.delete('/api/todos/:todo_id',function(req,res){
+	Todo.remove({
+		_id: req.params.todo_id
+	}, function(err,todo){
+		if(err)
+			res.send(err);
+
+		Todo.find(function(err,todos){
+			if(err)
+				res.send(err)
+			res.json(todos);
+		});
+	}
+}
+
+
+
+
+
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
